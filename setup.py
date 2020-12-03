fcff897acf5993e2a10aba7d23757c462ee8d8a9
Thomas Gläßle <t_glaessle@gmx.de>
2020-12-03 12:25:51 +0100

    Use version and other metadata from package.json

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 import setuptools
+import json
+
+with open('package.json') as f:
+    package_metadata = json.load(f)
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='pypi-register',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
-    scripts=['scripts/pypi-register']
+    scripts=['scripts/pypi-register'],
+    version=package_metadata['version'],
+    description=package_metadata['description'],
+    keywords=package_metadata['keywords'],
+    url=package_metadata['homepage'],
+    license=package_metadata['license'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
