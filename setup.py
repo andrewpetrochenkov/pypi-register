1b0a5649d8bbd9f2e0d23c0b810324b44acc407d
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 20:20:13 +0300

    2020:12:03 20:20

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pypi-register',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/pypi-register']
